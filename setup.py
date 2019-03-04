1d54aa97e0e953449b92508c8de3c5200bba75de
enblacar <enblacar1996@gmail.com>
2019-03-04 09:51:56 +0100

    changed github url in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='thewhitetools',
       version='0.1',
       description='A collection of handy functions and classes for a bioinformatician.',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/enblacar/thewhitetools',
       author='Enrique Blanco Carmona',
       author_email='enblacar1996@gmail.com',
       packages=['thewhitetools'],
