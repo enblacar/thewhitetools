38138f23ae874e2e965e9a3a211318039fc4ce9c
enblacar <enblacar1996@gmail.com>
2019-03-04 12:39:01 +0100

    new version

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(name='thewhitetools',
-      version='0.1',
-      description='A collection of handy functions and classes for a bioinformatician.',
-      url='https://github.com/enblacar/thewhitetools',
-      author='Enrique Blanco Carmona',
-      author_email='enblacar1996@gmail.com',
-      packages=['thewhitetools'],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="thewhitetools",
+    version="0.0.1",
+    author="Enrique Blanco Carmona",
+    author_email="enblacar1996@gmail.com",
+    description="A collection of functions and classes that come in handy for bioinformaticians.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/enblacar/thewhitetools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
