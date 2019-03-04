bccdb1a60aaccf06285cfd81e457d32abda8fcbc
enblacar <enblacar1996@gmail.com>
2019-03-04 09:49:38 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='thewhitetools',
+      version='0.1',
+      description='A collection of handy functions and classes for a bioinformatician.',
+      url='http://github.com/storborg/funniest',
+      author='Enrique Blanco Carmona',
+      author_email='enblacar1996@gmail.com',
+      packages=['thewhitetools'],
+      zip_safe=False)
